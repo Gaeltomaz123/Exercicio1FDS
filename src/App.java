d81b4d5ee23fa58201d9caba48fb1e1a80516a7d
Gaeltomaz123 <gaeltomaz@gmail.com>
2024-08-19 00:51:09 -0300

    Exercicio 2 feito

--- src/App.java
+++ src/App.java
@@ -2,10 +2,15 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
+        Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
+        Carro utilitario = new Carro("Utilitario", TipoCombustivel.DIESEL, 5, 70);
 
         System.out.println("Tipos de veiculos:");
         System.out.println(basico);
+        System.out.println(esportivo);
+        System.out.println(utilitario);
 
+        // Basico
         System.out.println("\n\n----------------");
         System.out.println("\nAbastencendo carro basico com gasolina");
         basico.abastece(TipoCombustivel.GASOLINA, 55);
@@ -16,17 +21,24 @@ public class App {
         System.out.println(basico);
 
 
-        // Atividade 1
-        Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
-        System.out.println(esportivo);
-
+        // Esportivo
         System.out.println("\n\n----------------");
         System.out.println("\nAbastencendo carro esportivo com gasolina");
-        esportivo.abastece(TipoCombustivel.GASOLINA, 40);
+        esportivo.abastece(TipoCombustivel.GASOLINA, 45);
         System.out.println(esportivo);
-
-        System.out.println("\nViajando com o esportivo basico");
+        System.out.println("\nViajando com o carro esportivo");
         esportivo.viaja(100);
         System.out.println(esportivo);
+
+        // Utilitario
+        System.out.println("\n\n----------------");
+        System.out.println("\nAbastencendo carro utilitario com diesel");
+        utilitario.abastece(TipoCombustivel.DIESEL, 70);
+        System.out.println(utilitario);
+        System.out.println("\nViajando com o utilitario basico");
+        utilitario.viaja(250);
+        utilitario.viaja(150);
+        System.out.println(utilitario);
+
     }
 }
